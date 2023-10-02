6f071390eafa389a9bcd3db611f80b81b6dc76ca
Marian <roshchupkin.marian@gmail.com>
2023-10-02 22:11:48 +1000

    initialized frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './main.global.css';
+import {Layout} from "./shared/Layout";
+import {Content} from "./shared/Content";
 
-function App() {
+function AppComponent() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Content>
+        <div>Hello world!</div>
+      </Content>
+    </Layout>
   );
 }
 
-export default App;
+export const App = () => <AppComponent />
---
+++ frontend/src/shared/Layout/Layout.tsx
@@ -0,0 +1,14 @@
+import React, {ReactNode} from 'react';
+import styles from './layout.module.css';
+
+interface ILayout {
+  children?: ReactNode;
+}
+
+export function Layout({ children }: ILayout) {
+  return (
+    <div className={styles.layout}>
+      {children}
+    </div>
+  );
+}
